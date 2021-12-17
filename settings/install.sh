dfae12784bb1b73a6fb304b5c826bcd2bdd2d6c4
Crazyfire <fdff87554@gmail.com>
2021-12-17 10:23:06 +0800

    fix install database

--- settings/install.sh
+++ settings/install.sh
@@ -4,6 +4,6 @@ apt update
 apt upgrade -y
 apt install apache2 -y
 ufw allow "Apache"
-apt install mariadb-server -y
+apt install mysql-server -y
 apt install php libapache2-mod-php php-mysql -y
 systemctl restart apache2
